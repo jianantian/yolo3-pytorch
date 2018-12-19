b0206db387e56f5c937983c0c43cc27cd138faa3
emile <emile.zhu@hotmail.com>
2018-12-19 17:11:35 +0800

    update MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open('requirements.txt') as fr:
     required = fr.read().splitlines()
 
 setup(
-        name='yolo',
-        version='0.1.0',
-        install_requires=required,
-        packages=find_packages(),
-        url='',
-        author='jianantian',
-        author_email='emile.zhu@hotmail.com',
-        zip_safe=False,
-        description='object detection by yolo3'
+    name='yolo',
+    version='0.1.0',
+    install_requires=required,
+    packages=['yolo', 'resource', 'cfg'],
+    include_package_data=True,
+    url='',
+    author='emile',
+    author_email='emile.zhu@hotmail.com',
+    zip_safe=False,
+    license='BSD 3-Clause',
+    description='object detection by yolo3'
 )
